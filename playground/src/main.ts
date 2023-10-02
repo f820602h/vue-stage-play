27f41d891400cf2ecdd0443d0393eae6cdb3f804
max.lee <f820602h@gmail.com>
2023-10-02 10:25:25 +0800

    [remove] remove playground

--- playground/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import { StagePlayPlugin } from "../../src/index";
-
-import App from "./App.vue";
-const app = createApp(App);
-app.use(StagePlayPlugin());
-
-app.mount("#app");
